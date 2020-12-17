2278f716a82c2a04e38c9146d698d2ec0a285960
arnu515 <arnu5152@gmail.com>
2020-12-17 12:08:02 +0530

    add Post interface

--- src/util/types.ts
+++ src/util/types.ts
@@ -12,3 +12,13 @@ export interface User {
     created_at: string;
     updated_at: string;
 }
+
+export interface Post {
+    id: number;
+    title: string;
+    public: boolean;
+    suggestions: boolean;
+    created_at: string;
+    updated_at: string;
+    user: User;
+}
