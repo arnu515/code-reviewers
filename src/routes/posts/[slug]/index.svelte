df717525eb34b23eb34c4ed3ac8e3e003f26cda1
arnu515 <arnu5152@gmail.com>
2020-12-21 12:51:10 +0530

    reviews

--- src/routes/posts/[slug]/index.svelte
+++ src/routes/posts/[slug]/index.svelte
@@ -2,12 +2,15 @@
     import axios from "axios";
     import type { Post, Resp } from "../../../util/types";
 
-    export async function preload({ params: { slug } }, { apiUrl }) {
+    export async function preload(
+        { params: { slug } },
+        { apiUrl, accessToken }
+    ) {
         try {
             const { data } = await axios.get<Resp<{ post: Post }>>(
                 apiUrl + "/api/posts/" + slug
             );
-            return { post: data.data.post, apiUrl };
+            return { post: data.data.post, apiUrl, accessToken };
         } catch (e) {
             if (e?.response?.status === 404) this.error(404, "Post not found");
         }
@@ -18,12 +21,34 @@
     import { user } from "../../../util/stores";
     import { onMount, tick } from "svelte";
     import Loading from "../../../components/Loading.svelte";
-    import type { Code } from "../../../util/types";
+    import type { Code, Review } from "../../../util/types";
     import { handleAxiosError } from "../../../util";
     import hljs from "highlight.js";
+    import { fade } from "svelte/transition";
 
     export let post: Post;
     export let apiUrl: string;
+    export let accessToken: string;
+
+    // false = reviews, true = suggestions
+    let reviewsSuggestions = false;
+
+    let reviewTitle: string,
+        reviewContent: string,
+        reviewStars = 0,
+        reviewError: string;
+
+    async function getReviews() {
+        try {
+            const { data } = await axios.get<Resp<{ reviews: Review[] }>>(
+                apiUrl + "/api/reviews/" + post.id
+            );
+            return data.data.reviews;
+        } catch (e) {
+            console.error(e);
+            alert(handleAxiosError(e));
+        }
+    }
 
     onMount(() => {
         document.querySelectorAll(".code").forEach(async (el) => {
@@ -54,6 +79,31 @@
             }
         });
     });
+
+    async function addReview() {
+        if (!reviewTitle.trim() || !reviewContent.trim() || !reviewStars)
+            reviewError = "Fill out all fields!";
+        else reviewError = "";
+
+        if (reviewError) return;
+
+        try {
+            const { data } = await axios.post<Resp<{ review: Review }>>(
+                apiUrl + "/api/reviews/" + post.id,
+                {
+                    title: reviewTitle.trim(),
+                    content: reviewContent.trim(),
+                    stars: reviewStars,
+                },
+                { headers: { Authorization: "Bearer " + accessToken } }
+            );
+            if (data.success) window.location.reload();
+        } catch (e) {
+            if ([401, 403, 422].includes(e?.response?.status))
+                window.location.href = "/logout";
+            else reviewError = handleAxiosError(e);
+        }
+    }
 </script>
 
 <style>
@@ -66,6 +116,31 @@
         box-shadow: 2px 5px 15px 3px rgba(0, 0, 0, 0.23);
         -webkit-box-shadow: 2px 5px 15px 3px rgba(0, 0, 0, 0.23);
         -moz-box-shadow: 2px 5px 15px 3px rgba(0, 0, 0, 0.23);
+    }
+
+    .nav-link:hover {
+        cursor: pointer;
+    }
+
+    .stars-selector {
+        display: flex;
+        gap: 1ch;
+        color: rgb(255, 193, 7);
+        color: rgb(255, 193, 7, 0.4);
+    }
+
+    .stars-selector i {
+        transition: 200ms color ease;
+    }
+
+    .stars-selector i:hover {
+        color: rgb(255, 193, 7);
+        color: rgb(255, 193, 7, 0.8);
+        cursor: pointer;
+    }
+
+    .stars-selector i.active {
+        color: #ffc107;
     }
 </style>
 
@@ -127,4 +202,137 @@
 
 <hr />
 
-<h3 id="reviews">Reviews</h3>
+<div class="mt-3" id="reviews">
+    <ul class="nav nav-tabs">
+        <li class="nav-item">
+            <a
+                class="nav-link"
+                href="#reviews"
+                class:active={!reviewsSuggestions}
+                role="link"
+                on:click|preventDefault={() => (reviewsSuggestions = false)}>Reviews</a>
+        </li>
+        <li class="nav-item">
+            <a
+                class="nav-link"
+                href="#suggestions"
+                class:active={reviewsSuggestions}
+                role="link"
+                on:click|preventDefault={() => (reviewsSuggestions = true)}>Suggestions</a>
+        </li>
+    </ul>
+</div>
+
+{#if !reviewsSuggestions}
+    <div class="m-2" in:fade on:submit|preventDefault={addReview}>
+        {#if reviewError}
+            <div class="alert alert-danger">{reviewError}</div>
+        {/if}
+        <h3 class="d-flex w-100 align-items-center">
+            Reviews
+            <button
+                class="btn btn-outline-dark ms-auto"
+                data-bs-toggle="collapse"
+                data-bs-target="#new-review">New review</button>
+        </h3>
+        <div class="collapse" id="new-review">
+            <div class="card my-3">
+                <div class="card-header">New review</div>
+                <div class="card-body">
+                    <form>
+                        <p>
+                            <label for="title">Title</label>
+                            <input
+                                type="text"
+                                class="form-control"
+                                bind:value={reviewTitle}
+                                id="title" />
+                        </p>
+                        <div class="mt-1 mb-2">
+                            <label for="stars">Stars
+                                <span class="ms-2">({reviewStars} / 5)</span>
+                            </label>
+                            <div class="stars-selector">
+                                <i
+                                    class="fas fa-star"
+                                    class:active={reviewStars > 0}
+                                    on:click={() => {
+                                        reviewStars = 1;
+                                    }} />
+                                <i
+                                    class="fas fa-star"
+                                    class:active={reviewStars > 1}
+                                    on:click={() => {
+                                        reviewStars = 2;
+                                    }} />
+                                <i
+                                    class="fas fa-star"
+                                    class:active={reviewStars > 2}
+                                    on:click={() => {
+                                        reviewStars = 3;
+                                    }} />
+                                <i
+                                    class="fas fa-star"
+                                    class:active={reviewStars > 3}
+                                    on:click={() => {
+                                        reviewStars = 4;
+                                    }} />
+                                <i
+                                    class="fas fa-star"
+                                    class:active={reviewStars > 4}
+                                    on:click={() => {
+                                        reviewStars = 5;
+                                    }} />
+                            </div>
+                        </div>
+                        <p>
+                            <label for="content">Review content</label>
+                            <textarea
+                                id="content"
+                                class="form-control"
+                                bind:value={reviewContent}
+                                rows="4" />
+                        </p>
+                        <p>
+                            <button class="btn btn-success" type="submit">Create
+                                review</button>
+                        </p>
+                    </form>
+                </div>
+            </div>
+        </div>
+        {#await getReviews()}
+            <Loading message="Loading reviews" />
+        {:then reviews}
+            {#if reviews && reviews.length > 0}
+                {#each reviews as r}
+                    <div class="card mt-2">
+                        <div class="card-header d-flex align-items-center">
+                            @{r.user.username}
+                            <span
+                                class="ms-auto text-muted">{new Date(r.created_at)}</span>
+                        </div>
+                        <div class="card-body">
+                            <div
+                                class="d-flex w-100 align-items-center justify-content-between">
+                                <h4>{r.title}</h4>
+                                <div>
+                                    <span>{@html new Array(5)
+                                            .fill(0)
+                                            .map((_, y) => {
+                                                if (y <= r.stars - 1) return `<i class="fas fa-star text-warning"></i>`;
+                                                else return `<i class="far fa-star text-warning"></i>`;
+                                            })
+                                            .join(' ')}</span>
+                                </div>
+                            </div>
+                            <div class="mt-2">{r.content}</div>
+                        </div>
+                    </div>
+                {/each}
+            {:else}
+                <p>No reviews yet!</p>
+            {/if}
+        {/await}
+    </div>
+{/if}
